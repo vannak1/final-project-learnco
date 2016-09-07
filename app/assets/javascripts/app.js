5a863b8ca8b299941785b10405314f32efa6c2fb
Vannak Chhay <vannak.chhay1@gmail.com>
2016-09-07 00:03:05 -0400

    pending route

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,11 +1,33 @@
 angular.module('learnExchange', [])
+  .factory('questions', [function(){
+    var service = {
+      questions: [
+        {body: 'post1', lessonLink: 'www.learn.co', githubLink: 'github.com', votes: 1},
+        {body: 'post2', lessonLink: 'www.learn.co', githubLink: 'github.com', votes: 2},
+        {body: 'post3', lessonLink: 'www.learn.co', githubLink: 'github.com', votes: 3},
+        {body: 'post4', lessonLink: 'www.learn.co', githubLink: 'github.com', votes: 4},
+        {body: 'post5', lessonLink: 'www.learn.co', githubLink: 'github.com', votes: 5}
+      ]
+    };
+    return service;
+  }])
+  .controller('MainController', ['$scope', 'questions', function($scope, questions){
+    $scope.questions = questions.questions;
 
-  .controller('MainController', ['$scope', function($scope){
-    $scope.questions = [
-      {body: 'post1', lessonLink: 'www.learn.co', githubLink: 'github.com', votes: 1},
-      {body: 'post2', lessonLink: 'www.learn.co', githubLink: 'github.com', votes: 2},
-      {body: 'post3', lessonLink: 'www.learn.co', githubLink: 'github.com', votes: 3},
-      {body: 'post4', lessonLink: 'www.learn.co', githubLink: 'github.com', votes: 4},
-      {body: 'post5', lessonLink: 'www.learn.co', githubLink: 'github.com', votes: 5}
-    ];
+    $scope.addQuestion = function(){
+      if(!$scope.body || $scope.body === '') { return; }
+      $scope.questions.push({
+          body:       $scope.body,
+          lessonLink: $scope.lessonLink,
+          githubLink: $scope.githubLink,
+          votes:      0
+        });
+        $scope.body = '';
+        $scope.lessonLink = '';
+        $scope.githubLink = '';
+    };
+
+    $scope.incrementUpvotes = function(question) {
+      question.votes += 1;
+    };
   }]);
